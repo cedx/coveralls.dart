f71366bb5f6819aba36f7ea25b1ec549e6207974
Cédric Belin <cedric@belin.io>
2019-09-19 20:24:04 +0200

    Added the build URL to GitHub service

--- lib/src/io/services/github.dart
+++ lib/src/io/services/github.dart
@@ -1,8 +1,15 @@
 import 'package:coveralls/coveralls.dart';
 
 /// Gets the [GitHub](https://github.com) configuration parameters from the specified environment.
-Configuration getConfiguration(Map<String, String> env) => Configuration({
-  'commit_sha': env['GITHUB_SHA'],
-  'service_branch': env['GITHUB_REF'],
-  'service_name': 'github'
-});
+Configuration getConfiguration(Map<String, String> env) {
+  final commitSha = env['GITHUB_SHA'];
+  final gitRef = env['GITHUB_REF'] ?? '';
+  final repository = env['GITHUB_REPOSITORY'];
+
+  return Configuration({
+    'commit_sha': commitSha,
+    'service_branch': gitRef.startsWith('refs/heads/') ? gitRef.substring(11) : null,
+    'service_build_url': commitSha != null && repository != null ? 'https://github.com/$repository/commit/$commitSha/checks' : null,
+    'service_name': 'github'
+  });
+}
