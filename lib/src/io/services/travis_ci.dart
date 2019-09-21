02abc0023f70cb6da1709a0bff167584e24fc06f
Cédric Belin <cedric@belin.io>
2019-09-21 03:29:41 +0200

    Improved the Travis CI support

--- lib/src/io/services/travis_ci.dart
+++ lib/src/io/services/travis_ci.dart
@@ -3,8 +3,10 @@ import 'package:coveralls/coveralls.dart';
 /// Gets the [Travis CI](https://travis-ci.com) configuration parameters from the specified environment.
 Configuration getConfiguration(Map<String, String> env) {
   final config = Configuration({
-    'commit_sha': 'HEAD',
+    'commit_sha': env['TRAVIS_COMMIT'],
+    'flag_name': env['TRAVIS_JOB_NAME'],
     'service_branch': env['TRAVIS_BRANCH'],
+    'service_build_url': env['TRAVIS_BUILD_WEB_URL'],
     'service_job_id': env['TRAVIS_JOB_ID'],
     'service_name': 'travis-ci'
   });
