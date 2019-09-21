8b2c45b74c5e7cc0f833c80df4c0362b81dc3111
Cédric Belin <cedric@belin.io>
2019-09-21 03:29:14 +0200

    Improved the GitHub support

--- lib/src/io/services/github.dart
+++ lib/src/io/services/github.dart
@@ -3,12 +3,14 @@ import 'package:coveralls/coveralls.dart';
 /// Gets the [GitHub](https://github.com) configuration parameters from the specified environment.
 Configuration getConfiguration(Map<String, String> env) {
   final commitSha = env['GITHUB_SHA'];
-  final gitRef = env['GITHUB_REF'] ?? '';
   final repository = env['GITHUB_REPOSITORY'];
+
+  final gitRef = env['GITHUB_REF'] ?? '';
+  final gitRegex = RegExp('^refs/\w+/');
 
   return Configuration({
     'commit_sha': commitSha,
-    'service_branch': gitRef.startsWith('refs/heads/') ? gitRef.substring(11) : null,
+    'service_branch': gitRegex.hasMatch(gitRef) ? gitRef.replaceFirst(gitRegex, '') : null,
     'service_build_url': commitSha != null && repository != null ? 'https://github.com/$repository/commit/$commitSha/checks' : null,
     'service_name': 'github'
   });
