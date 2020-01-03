56e9a7a69146c709cc21974f0e9d2cc175443cfb
Cédric Belin <cedric@belin.io>
2020-01-03 18:21:09 +0100

    Updated the generated files

--- lib/src/io.g.dart
+++ lib/src/io.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'io.dart';
+part of coveralls.io;
 
 // **************************************************************************
 // JsonSerializableGenerator
