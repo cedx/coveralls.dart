1ad379529c872f4118db1d9e2d04d1d09b3efe1e
Cédric Belin <cedric@belin.io>
2020-02-05 23:36:39 +0100

    Added a VSCode launch configuration

--- lib/src/io/client.dart
+++ lib/src/io/client.dart
@@ -63,8 +63,7 @@ class Client {
   /// Completes with an [ArgumentError] if the job does not meet the requirements.
   /// Completes with a [http.ClientException] if the remote service does not respond successfully.
   Future<void> uploadJob(Job job) async {
-    if (job.repoToken == null && job.serviceName == null)
-      throw ArgumentError.value(job, 'job', 'The job does not meet the requirements.');
+    if (job.repoToken == null && job.serviceName == null) throw ArgumentError('The job does not meet the requirements.');
 
     final httpClient = http.Client();
     final request = http.MultipartRequest('POST', endPoint.resolve('jobs'))
